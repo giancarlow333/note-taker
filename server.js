a4ddfdf6b71dd018528d2f20d023843d13fc246d
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-30 15:39:49 -0700

    Set up GET API route for notes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // import required packages
 const express = require('express');
 const path = require('path');
+const theNotes = require('./db/db.json');
 
 const PORT = 3001;
 const app = express();
 app.use(express.static('/public'));
 
-// set up routes
+// set up html routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
@@ -15,7 +16,11 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-
+// set up API routes
+app.get('/api/notes', (req, res) => 
+    console.info(`GET /api/notes`);
+    res.status(200).json(theNotes);
+);
 
 
 
