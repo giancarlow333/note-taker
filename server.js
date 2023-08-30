c421a16edbc3cae18f41f7bcc98966b952be15ec
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-30 14:45:44 -0700

    GET routes set up

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const PORT = 3001;
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 // set up routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 
