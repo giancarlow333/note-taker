90fc3562db5f0388e9d184b143d6e3cab6bbd859
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-30 15:57:04 -0700

    Set up POST API route; add app.use(express.json())

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const theNotes = require('./db/db.json');
 const PORT = 3001;
 const app = express();
 app.use(express.static('/public'));
+app.use(express.json());
 
 // set up html routes
 app.get('/', (req, res) => {
@@ -17,12 +18,38 @@ app.get('/notes', (req, res) => {
 });
 
 // set up API routes
-app.get('/api/notes', (req, res) => 
+app.get('/api/notes', function (req, res) {
     console.info(`GET /api/notes`);
     res.status(200).json(theNotes);
-);
+});
 
-
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    console.log(`Request body: ${req.body}`);
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text
+      };
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+});
 
 
 app.listen(PORT, () =>
