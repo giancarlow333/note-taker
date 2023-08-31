94056287384e478232369c42070ea5ea29514d2f
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-30 22:39:31 -0700

    Remove declaration of PORT = 3001

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-//const PORT = process.env.PORT;
-const PORT = 3001;
+const PORT = process.env.PORT;
+//const PORT = 3001;
 const app = express();
 app.use(express.static('./public'));
 app.use(express.json());
